382cd8969791f6d7a8bf66d2a822e66f08cd16b1
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:22:11 +0800

    Add project files.

---
+++ Exercise8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Exercise8
+{
+	public static void Main()
+	{
+		int num1, num2, num3;
+		Console.Write("Find the largest of three numbers:\n");
+		Console.Write("Input the 1st number :");
+		num1 = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Input the  2nd number :");
+		num2 = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Input the 3rd  number :");
+		num3 = Convert.ToInt32(Console.ReadLine());
+		if (num1 > num2)
+		{
+			if (num1 > num3)
+			{
+				Console.Write("The 1st Number is the greatest among three");
+			}
+			else
+			{
+				Console.Write("The 3rd Number is the greatest among three");
+			}
+		}
+		else if (num2 > num3)
+			Console.Write("The 2nd Number is the greatest among three");
+		else
+			Console.Write("The 3rd Number is the greatest among three");
+	}
+}
